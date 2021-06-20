996a011be7c533e4ae8af16570c3bd2ae591c8b0
Kunal <kunalshetti.u@gmail.com>
2021-06-20 18:22:30 +0530

    Activity Layout created with DataBinding

--- app/src/main/java/myapp/kshetti/shaadiassignment/ui_controllers/MainActivity.kt
+++ app/src/main/java/myapp/kshetti/shaadiassignment/ui_controllers/MainActivity.kt
@@ -2,18 +2,25 @@ package myapp.kshetti.shaadiassignment.ui_controllers
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
+import myapp.kshetti.shaadiassignment.databinding.ActivityMainBinding
 import myapp.kshetti.shaadiassignment.view_models.ProfileViewModel
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     private val profileViewModel: ProfileViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater())
     }
 }
