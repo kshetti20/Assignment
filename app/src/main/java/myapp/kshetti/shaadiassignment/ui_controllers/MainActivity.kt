609510045a1d1500024d38b85b94d4c3cabde610
Kunal <kunalshetti.u@gmail.com>
2021-06-21 02:02:22 +0530

    Removed deprecated getDrawable

--- app/src/main/java/myapp/kshetti/shaadiassignment/ui_controllers/MainActivity.kt
+++ app/src/main/java/myapp/kshetti/shaadiassignment/ui_controllers/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View.GONE
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -35,19 +36,23 @@ class MainActivity : AppCompatActivity() {
             profileViewModel.userList.observe(this@MainActivity, Observer {
                 if (it.isNullOrEmpty()) {
                     binding.userProfileRV.visibility = GONE
-                    binding.backgroundIV.setImageDrawable(resources.getDrawable(R.drawable.no_internet))
+                    binding.backgroundIV.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this@MainActivity,
+                            R.drawable.no_internet
+                        ))
                 } else {
                     binding.backgroundIV.visibility = GONE
                     userProfileRV.adapter = UserProfileAdapter(it) {
                         profileViewModel.updateAcceptanceState(it.uID, it.status)
 
-                        //Latest acceptance modified position is considered as the last viewed profile
+                        //Latest acceptance state modified position is considered as the last viewed profile
+                        //If this position is the last, UI would display the 1st element
                         profileViewModel.saveViewedPosition(it.uID)
                     }
                     userProfileRV.scrollToPosition(profileViewModel.getViewedPosition().toInt())
                 }
             })
-
 
             if (profileViewModel.isNetworkConnected()) profileViewModel.insertAndFetchUsers()
             else profileViewModel.fetchStoredUsers()
