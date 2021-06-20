d4323d7513df79d4066b163f7daeb93a7096d1ce
Kunal <kunalshetti.u@gmail.com>
2021-06-20 22:18:19 +0530

    Profile row updated as per the acceptance status

--- app/src/main/java/myapp/kshetti/shaadiassignment/adapters/UserProfileAdapter.kt
+++ app/src/main/java/myapp/kshetti/shaadiassignment/adapters/UserProfileAdapter.kt
@@ -1,6 +1,8 @@
 package myapp.kshetti.shaadiassignment.adapters
 
 import android.view.LayoutInflater
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -26,27 +28,44 @@ class UserProfileAdapter(val results: List<Results>, val function: (Results) -> 
 
     override fun getItemCount() = results.size
 
-
     inner class ViewHolder(val binding: UserProfileRowBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bindView(profile: Results) {
             binding.apply {
-
-            Glide.with(profileIV.context)
-                .load(profile.picture.thumbnail)
-                .into(profileIV)
-
                 user = profile
+                Glide.with(profileIV.context)
+                    .load(profile.picture.thumbnail)
+                    .into(profileIV)
+
+                setAcceptanceView(profile.status)
 
                 acceptIV.setOnClickListener {
                     profile.status = 1
                     function.invoke(profile)
+                    setAcceptanceView(profile.status)
                 }
                 declineIV.setOnClickListener {
                     profile.status = -1
                     function.invoke(profile)
+                    setAcceptanceView(profile.status)
                 }
+            }
+        }
+
+        private fun UserProfileRowBinding.setAcceptanceView(status: Int) {
+
+            if (status != 0) {
+                acceptanceViewLL.visibility = GONE
+                acceptamceStatusTV.visibility = VISIBLE
+
+                when (status) {
+                    1 -> acceptamceStatusTV.text = "Accepted"
+                    -1 -> acceptamceStatusTV.text = "Declined"
+                }
+            }else{
+                acceptanceViewLL.visibility = VISIBLE
+                acceptamceStatusTV.visibility = GONE
             }
         }
     }
