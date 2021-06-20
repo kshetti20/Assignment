4dbf5297d17fcc0477be7e38ac5c25408f659f68
Kunal <kunalshetti.u@gmail.com>
2021-06-20 06:19:33 +0530

    LiveData observed for api response
    captured parsed api response
    
    postcode changed to String in Entity

--- app/src/main/java/myapp/kshetti/shaadiassignment/repositories/UserRepository.kt
+++ app/src/main/java/myapp/kshetti/shaadiassignment/repositories/UserRepository.kt
@@ -1,12 +1,23 @@
 package myapp.kshetti.shaadiassignment.repositories
 
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import myapp.kshetti.shaadiassignment.api_module.RestApi
 import myapp.kshetti.shaadiassignment.db_module.dao.UserDao
+import myapp.kshetti.trialapp.model.UserData
+import java.lang.reflect.Type
 import javax.inject.Inject
 
 
 class UserRepository @Inject constructor(
     @ActivityRetainedScoped val userDao: UserDao,
     @ActivityRetainedScoped val restApi: RestApi
-)
+) {
+    suspend fun getUsers(): UserData? {
+        val apiCall = restApi.getProfileList()
+        val users: UserData? = apiCall.execute().body()
+        return users
+    }
+}
--- app/src/main/java/myapp/kshetti/shaadiassignment/view_models/ProfileViewModel.kt
+++ app/src/main/java/myapp/kshetti/shaadiassignment/view_models/ProfileViewModel.kt
@@ -1,16 +1,29 @@
 package myapp.kshetti.shaadiassignment.view_models
 
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import myapp.kshetti.shaadiassignment.repositories.UserRepository
+import myapp.kshetti.trialapp.model.UserData
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor(savedStateHandle: SavedStateHandle): ViewModel() {
+class ProfileViewModel @Inject constructor(savedStateHandle: SavedStateHandle) : ViewModel() {
 
     @ViewModelScoped
     @Inject
     lateinit var userRepository: UserRepository
+
+    var data = MutableLiveData<UserData>()
+
+    fun getUsers() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                data.postValue(userRepository.getUsers())
+            }
+        }
+    }
 }
