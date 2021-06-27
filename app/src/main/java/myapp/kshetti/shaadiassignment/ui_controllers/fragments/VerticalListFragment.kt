867382b2e43f69b355915cade6bc005a824ccba6
Kunal <kunalshetti.u@gmail.com>
2021-06-27 12:40:48 +0530

    Inflated layout and data for Vertical List view

--- app/src/main/java/myapp/kshetti/shaadiassignment/ui_controllers/fragments/VerticalListFragment.kt
+++ app/src/main/java/myapp/kshetti/shaadiassignment/ui_controllers/fragments/VerticalListFragment.kt
@@ -1,14 +1,26 @@
 package myapp.kshetti.shaadiassignment.ui_controllers.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import myapp.kshetti.shaadiassignment.R
+import myapp.kshetti.shaadiassignment.adapters.UserCardAdapter
+import myapp.kshetti.shaadiassignment.adapters.UserProfileAdapter
 import myapp.kshetti.shaadiassignment.databinding.FragmentHorizontalListBinding
 import myapp.kshetti.shaadiassignment.databinding.FragmentVerticalListBinding
+import myapp.kshetti.shaadiassignment.view_models.ProfileViewModel
 
 class VerticalListFragment : Fragment() {
+
+    private val profileViewModel: ProfileViewModel by activityViewModels()
 
     private lateinit var binding: FragmentVerticalListBinding
 
@@ -21,5 +33,42 @@ class VerticalListFragment : Fragment() {
             inflater, container, false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+            userProfileRV.layoutManager = LinearLayoutManager(
+                activity,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+
+            profileViewModel.userList.observe(viewLifecycleOwner, Observer {
+                if (it.isNullOrEmpty()) {
+                    binding.userProfileRV.visibility = View.GONE
+                    binding.backgroundIV.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            activity as Context,
+                            R.drawable.no_internet
+                        )
+                    )
+                } else {
+                    binding.backgroundIV.visibility = View.GONE
+                    userProfileRV.adapter = UserCardAdapter(it) {
+                        profileViewModel.updateAcceptanceState(it.uID, it.status)
+
+                        //Latest acceptance state modified position is considered as the last viewed profile
+                        //If this position is the last, UI would display the 1st element
+                        profileViewModel.saveViewedPosition(it.uID)
+                    }
+                    userProfileRV.scrollToPosition(profileViewModel.getViewedPosition().toInt())
+                }
+            })
+
+            if (profileViewModel.isNetworkConnected()) profileViewModel.insertAndFetchUsers()
+            else profileViewModel.fetchStoredUsers()
+        }
     }
 }
